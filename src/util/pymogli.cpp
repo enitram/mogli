a22793e750b0087a084007f9df30af745608d851
Martin Engler <martin.engler@gmail.com>
2019-07-03 11:33:56 +0200

    refactored python bindings from boost to pybind11

---
+++ src/util/pymogli.cpp
@@ -0,0 +1,198 @@
+//
+// Created by martin on 7/1/19.
+//
+
+#include <pybind11/iostream.h>
+#include <pybind11/operators.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
+#include "../../include/canonization.h"
+#include "../../include/mcf.h"
+#include "../../include/util/packing.h"
+
+namespace py = pybind11;
+
+PYBIND11_MAKE_OPAQUE(mogli::BoolVector);
+PYBIND11_MAKE_OPAQUE(mogli::ShortVector);
+PYBIND11_MAKE_OPAQUE(mogli::IntVector);
+PYBIND11_MAKE_OPAQUE(mogli::LongVector);
+PYBIND11_MAKE_OPAQUE(mogli::StringVector);
+PYBIND11_MAKE_OPAQUE(mogli::FragmentVector);
+PYBIND11_MAKE_OPAQUE(mogli::MatchVector);
+PYBIND11_MAKE_OPAQUE(mogli::SharedPtrVector<mogli::Molecule>::type)
+PYBIND11_MAKE_OPAQUE(mogli::NodeVector);
+
+inline py::object pass_through(py::object const& o) { return o; }
+
+template <typename T, typename E>
+struct ItWrapper {
+
+  static E next(T& o) {
+    E next = o;
+    if (next == lemon::INVALID) {
+      PyErr_SetString(PyExc_StopIteration, "No more data.");
+      throw py::error_already_set();
+    }
+    ++o;
+    return next;
+  }
+
+  static void wrap(py::handle handle, const char * name) {
+    py::class_<T>(handle, name)
+        .def("next", next)
+        .def("__iter__", pass_through);
+  }
+};
+
+PYBIND11_MODULE(mogli, m) {
+
+  // gaph iterators
+
+  ItWrapper<mogli::EdgeIt, const mogli::Edge>::wrap(m, "EdgeIt");
+  ItWrapper<mogli::IncEdgeIt, const mogli::Edge>::wrap(m, "IncEdgeIt");
+  ItWrapper<mogli::NodeIt, const mogli::Node>::wrap(m, "NodeIt");
+
+  // STL containers
+
+  py::bind_vector<mogli::BoolVector>(m, "BoolVector");
+  py::bind_vector<mogli::ShortVector>(m, "ShortVector", py::buffer_protocol());
+  py::bind_vector<mogli::IntVector>(m, "IntVector", py::buffer_protocol());
+  py::bind_vector<mogli::LongVector>(m, "LongVector", py::buffer_protocol());
+  py::bind_vector<mogli::StringVector>(m, "StringVector");
+  py::bind_vector<mogli::FragmentVector>(m, "FragmentVector");
+  py::bind_vector<mogli::MatchVector>(m, "MatchVector");
+  py::bind_vector<mogli::SharedPtrVector<mogli::Molecule>::type>(m, "MoleculeVector");
+  py::bind_vector<mogli::NodeVector>(m, "NodeVector");
+
+  // enums
+
+  py::enum_<mogli::Product::GenerationType>(m, "GenerationType")
+      .value("NO_OPT", mogli::Product::GenerationType::NO_OPT, "No optimization")
+      .value("UNCON", mogli::Product::GenerationType::UNCON, "Graph partitioning rule")
+      .value("DEG_1", mogli::Product::GenerationType::DEG_1, "Degree-1 neighbors rule")
+      .value("UNCON_DEG_1", mogli::Product::GenerationType::UNCON_DEG_1, "Degree-1 neighbors and graph partitioning rules");
+
+  // classes
+
+  py::class_<mogli::Canonization>(m, "Canonization")
+      .def(py::init<const mogli::Molecule&>())
+      .def("get_colors", &mogli::Canonization::get_colors, py::return_value_policy::move, "Get element numbers of the ordered atoms")
+      .def("get_canonization", &mogli::Canonization::get_canonization, py::return_value_policy::move, "Get canonization")
+      .def("get_node_order", &mogli::Canonization::get_node_order, py::return_value_policy::move, "Get ordered atom ids")
+      .def("is_isomorphic", &mogli::Canonization::is_isomorphic, "Isomorphism test");
+
+  py::class_<mogli::Edge>(m, "Edge")
+      .def(py::self == py::self)
+      .def(py::self != py::self)
+      .def(py::self < py::self);
+
+  py::class_<mogli::FragmentCanonization, mogli::Canonization>(m, "FragmentCanonization")
+      .def(py::init<const mogli::Fragment&>())
+      .def("get_core_nodes", &mogli::FragmentCanonization::get_core_nodes, py::return_value_policy::move, "Get atom ids of the ordered core atoms")
+      .def("is_isomorphic", &mogli::FragmentCanonization::is_isomorphic, "Isomorphism test");
+
+  py::class_<mogli::LGFIOConfig>(m, "LGFIOConfig")
+      .def(py::init<std::string, std::string>())
+      .def("add_bool_node_prop", &mogli::LGFIOConfig::add_bool_node_prop, py::return_value_policy::reference_internal, "Add a node property that will be cast to a bool")
+      .def("add_int_node_prop", &mogli::LGFIOConfig::add_int_node_prop, py::return_value_policy::reference_internal, "Add a node property that will be cast to an int")
+      .def("add_double_node_prop", &mogli::LGFIOConfig::add_double_node_prop, py::return_value_policy::reference_internal, "Add a node property that will be cast to a double")
+      .def("add_string_node_prop", &mogli::LGFIOConfig::add_string_node_prop, py::return_value_policy::reference_internal, "Add a node property that will be cast to a str");
+
+  py::class_<mogli::Match, std::shared_ptr<mogli::Match>>(m, "Match")
+      .def("frag_to_mol", &mogli::Match::frag_to_mol)
+      .def("merged_frag_to_mol", &mogli::Match::merged_frag_to_mol)
+      .def("get_atom_ids", &mogli::Match::get_atom_ids)
+      .def("map_ids", &mogli::Match::map_ids);
+
+  py::class_<mogli::Molecule, std::shared_ptr<mogli::Molecule>> molecule(m, "Molecule");
+  molecule.def(py::init<>())
+      .def(py::init<mogli::PeriodicTable&>())
+      .def("add_atom", py::overload_cast<std::string>(&mogli::Molecule::add_atom), "Add atom with element string")
+      .def("add_atom", py::overload_cast<int, std::string>(&mogli::Molecule::add_atom), "Add atom with element string and node id")
+      .def("add_atom", py::overload_cast<unsigned short>(&mogli::Molecule::add_atom), "Add atom with element number")
+      .def("add_atom", py::overload_cast<int, unsigned short>(&mogli::Molecule::add_atom), "Add atom with element number and node id")
+      .def("add_edge", &mogli::Molecule::add_edge, "Add bond")
+      .def("get_node_iter", &mogli::Molecule::get_node_iter, "Get atom iterator")
+      .def("get_edge_iter", &mogli::Molecule::get_edge_iter, "Get bond iterator")
+      .def("get_inc_edge_iter", &mogli::Molecule::get_inc_edge_iter, "Get incident bonds iterator")
+      .def("get_opposite_node", &mogli::Molecule::get_opposite_node, "Get atom on the opposite side of the bond")
+      .def("get_u", &mogli::Molecule::get_u, "Get first atom of the bond")
+      .def("get_v", &mogli::Molecule::get_v, "Get second atom of the bond")
+      .def("get_atom_count", &mogli::Molecule::get_atom_count, "Get number of atoms")
+      .def("has_node_with_id", &mogli::Molecule::has_node_with_id, "Tests if atom with this id exists")
+      .def("get_node_by_id", &mogli::Molecule::get_node_by_id, "Get atom by id")
+      .def("get_id", &mogli::Molecule::get_id, "Get id of this atom")
+      .def("get_color", &mogli::Molecule::get_color, "Get element number of this atom")
+      .def("get_element", &mogli::Molecule::get_element, "Get element of this atom")
+      .def("is_connected", &mogli::Molecule::is_connected, "Test if molecular graph is connected")
+      .def("get_connected_components", &mogli::Molecule::get_connected_components, "Get connected components of the molecular graph")
+      .def("split", &mogli::Molecule::split, "Balanced split of the molecule")
+      .def("is_isomorphic", &mogli::Molecule::is_isomorphic, "Isomorphisms test")
+      .def("write_lgf", py::overload_cast<>(&mogli::Molecule::write_lgf), "Write default formatted LGF file")
+      .def("write_lgf", py::overload_cast<const mogli::LGFIOConfig&>(&mogli::Molecule::write_lgf), "Write LGF file")
+      .def("read_lgf", py::overload_cast<const std::string &>(&mogli::Molecule::read_lgf), "Read default formatted LGF file")
+      .def("read_lgf", py::overload_cast<const std::string &, const mogli::LGFIOConfig&>(&mogli::Molecule::read_lgf), "Read LGF file")
+      .def("print_dot", py::overload_cast<>(&mogli::Molecule::print_dot, py::const_), "Print dot (graphviz file)")
+      .def("print_dot", py::overload_cast<const mogli::StringVector&>(&mogli::Molecule::print_dot, py::const_), "Print dot (graphviz) file with atom properties")
+      .def("set_property", &mogli::Molecule::set_property, "Set atom property")
+      .def("get_property", &mogli::Molecule::get_property, "Get atom property");
+
+  py::class_<mogli::Fragment, std::shared_ptr<mogli::Fragment>>(m, "Fragment", molecule)
+      .def("get_core_atom_count", &mogli::Fragment::get_core_atom_count, "Get number of core atoms")
+      .def("is_core", &mogli::Fragment::is_core, "Is this node a core atom?")
+      .def("print_dot", py::overload_cast<>(&mogli::Fragment::print_dot,  py::const_), "Print dot (graphviz) file");
+
+  py::class_<mogli::PeriodicTable>(m, "PeriodicTable")
+      .def(py::init<>())
+      .def("add", &mogli::PeriodicTable::add, py::return_value_policy::reference_internal, "Add a new element")
+      .def("add_uncolored", &mogli::PeriodicTable::add_uncolored, py::return_value_policy::reference_internal, "Add a new element with an associated color string (for dot printing)");
+
+  py::class_<mogli::Node>(m, "Node")
+      .def(py::self == py::self)
+      .def(py::self != py::self)
+      .def(py::self < py::self);
+
+  // methods
+
+  m.def("atomic_fragments", &mogli::atomic_fragments, "Get all single-atom fragments");
+
+  m.def("pack_canonization", &mogli::pack_canonization, "Serialize molecule canonization");
+
+  m.def("pack_fcanonization", &mogli::pack_fcanonization, "Serialize fragment canonization");
+
+  m.def("hash_canonization", &mogli::hash_canonization, "Get molecule canonization hash");
+
+  m.def("maximal_common_fragments", py::overload_cast<
+      mogli::Molecule&, mogli::Molecule&, mogli::FragmentVector&, mogli::MatchVector&, mogli::MatchVector&,
+      int, unsigned int, unsigned int, mogli::Product::GenerationType, bool, bool, int>(&mogli::maximal_common_fragments),
+      "Compute maximal common fragments");
+
+  m.def("maximal_common_fragments", py::overload_cast<
+      mogli::Molecule&, mogli::Molecule&, mogli::FragmentVector&, mogli::MatchVector&, mogli::MatchVector&,
+      int, unsigned int, mogli::Product::GenerationType, bool, bool, int>(&mogli::maximal_common_fragments),
+      "Compute maximal common fragments");
+
+  m.def("hash_fcanonization", &mogli::hash_fcanonization, "Get fragment canonization hash");
+
+  m.def("pack_fragment", py::overload_cast<const mogli::Fragment&>(&mogli::pack_fragment), "Serialize a fragment");
+
+  m.def("pack_fragment", py::overload_cast<const std::shared_ptr<mogli::Fragment> &>(&mogli::pack_fragment), "Serialize a fragment");
+
+  m.def("pack_match", &mogli::pack_match, "Serialize match object");
+
+  m.def("pack_molecule", py::overload_cast<const mogli::Molecule&>(&mogli::pack_molecule), "Serialize molecule");
+
+  m.def("pack_molecule", py::overload_cast<const std::shared_ptr<mogli::Molecule> &>(&mogli::pack_molecule), "Serialize molecule");
+
+  m.def("unpack_canonization", &mogli::unpack_canonization, "Deserialize molecule canonization");
+
+  m.def("unpack_fcanonization", &mogli::unpack_fcanonization, "Deserizalize fragment canonization");
+
+  m.def("unpack_fragment", &mogli::unpack_fragment, "Deserizalize fragment");
+
+  m.def("unpack_match", &mogli::unpack_match, "Deserialize match object");
+
+  m.def("unpack_molecule", &mogli::unpack_molecule, "Deserialize molecule");
+
+}
