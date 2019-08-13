79703dd2ca0ac0c90769674a63b736a01e7eb785
Martin Engler <martin.engler@gmail.com>
2019-08-13 12:05:04 +0200

    python matching unit tests

--- tests/test_python.py
+++ tests/test_python.py
@@ -652,9 +652,197 @@ class TestIsomorphism(unittest.TestCase):
 
 class TestMatching(unittest.TestCase):
 
-    pass
+    def test_mcf_isomorphic_graphs(self):
+        from mogli import Molecule, maximal_common_fragments, GenerationType
 
-    # TODO python matching tests
+        mol1, mol2 = Molecule(), Molecule()
+
+        mol1.read_lgf(ETHANE_1)
+        mol2.read_lgf(ETHANE_2)
+
+        frag_noopt, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.NO_OPT, False, TIMEOUT)
+        frag_deg1, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.DEG_1, False, TIMEOUT)
+        frag_uncon, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON, False, TIMEOUT)
+        frag_uncon_deg1, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON_DEG_1, False, TIMEOUT)
+
+        self.assertTrue(len(frag_noopt) > 0)
+        self.assertTrue(len(frag_deg1) > 0)
+        self.assertTrue(len(frag_uncon) > 0)
+        self.assertTrue(len(frag_uncon_deg1) > 0)
+
+        frag_noopt = sorted(frag_noopt, key=lambda x: x.get_atom_count())
+        frag_deg1 = sorted(frag_deg1, key=lambda x: x.get_atom_count())
+        frag_uncon = sorted(frag_uncon, key=lambda x: x.get_atom_count())
+        frag_uncon_deg1 = sorted(frag_uncon_deg1, key=lambda x: x.get_atom_count())
+
+        self.assertEqual(frag_noopt[0].get_atom_count(), mol1.get_atom_count())
+        self.assertEqual(frag_deg1[0].get_atom_count(), mol1.get_atom_count())
+        self.assertEqual(frag_uncon[0].get_atom_count(), mol1.get_atom_count())
+        self.assertEqual(frag_uncon_deg1[0].get_atom_count(), mol1.get_atom_count())
+
+    def test_mcf_isomorphic_graphs_max(self):
+        from mogli import Molecule, maximal_common_fragments, GenerationType
+
+        mol1, mol2 = Molecule(), Molecule()
+
+        mol1.read_lgf(ETHANE_1)
+        mol2.read_lgf(ETHANE_2)
+
+        frag_noopt, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.NO_OPT, True, TIMEOUT)
+        frag_deg1, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.DEG_1, True, TIMEOUT)
+        frag_uncon, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON, True, TIMEOUT)
+        frag_uncon_deg1, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON_DEG_1, True, TIMEOUT)
+
+        self.assertTrue(len(frag_noopt) > 0)
+        self.assertTrue(len(frag_deg1) > 0)
+        self.assertTrue(len(frag_uncon) > 0)
+        self.assertTrue(len(frag_uncon_deg1) > 0)
+
+        for f in frag_noopt:
+            self.assertEqual(f.get_atom_count(), mol1.get_atom_count())
+        for f in frag_deg1:
+            self.assertEqual(f.get_atom_count(), mol1.get_atom_count())
+        for f in frag_uncon:
+            self.assertEqual(f.get_atom_count(), mol1.get_atom_count())
+        for f in frag_uncon_deg1:
+            self.assertEqual(f.get_atom_count(), mol1.get_atom_count())
+
+    def test_mcf_isomorphic_graphs_big(self):
+        from mogli import Molecule, maximal_common_fragments, GenerationType
+
+        mol1, mol2 = Molecule(), Molecule()
+
+        mol1.read_lgf(MOLID3246)
+        mol2.read_lgf(MOLID3246)
+
+        frag_noopt, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.NO_OPT, False, TIMEOUT_BIG)
+        frag_deg1, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.DEG_1, False, TIMEOUT_BIG)
+        frag_uncon, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON, False, TIMEOUT_BIG)
+        frag_uncon_deg1, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON_DEG_1, False, TIMEOUT_BIG)
+
+        self.assertTrue(len(frag_noopt) > 0)
+        self.assertTrue(len(frag_deg1) > 0)
+        self.assertTrue(len(frag_uncon) > 0)
+        self.assertTrue(len(frag_uncon_deg1) > 0)
+
+        frag_noopt = sorted(frag_noopt, key=lambda x: x.get_atom_count())
+        frag_deg1 = sorted(frag_deg1, key=lambda x: x.get_atom_count())
+        frag_uncon = sorted(frag_uncon, key=lambda x: x.get_atom_count())
+        frag_uncon_deg1 = sorted(frag_uncon_deg1, key=lambda x: x.get_atom_count())
+
+        self.assertEqual(frag_noopt[0].get_atom_count(), mol1.get_atom_count())
+        self.assertEqual(frag_deg1[0].get_atom_count(), mol1.get_atom_count())
+        self.assertEqual(frag_uncon[0].get_atom_count(), mol1.get_atom_count())
+        self.assertEqual(frag_uncon_deg1[0].get_atom_count(), mol1.get_atom_count())
+
+    def test_mcf_isomorphic_graphs_large(self):
+        from mogli import Molecule, maximal_common_fragments, GenerationType
+
+        mol1, mol2 = Molecule(), Molecule()
+
+        mol1.read_lgf(PACLITAXEL)
+        mol2.read_lgf(PACLITAXEL)
+
+        frag_uncon, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON, False, TIMEOUT_LARGE)
+        frag_uncon_deg1, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON_DEG_1, False, TIMEOUT_LARGE)
+
+        self.assertTrue(len(frag_uncon) > 0)
+        self.assertTrue(len(frag_uncon_deg1) > 0)
+
+        frag_uncon = sorted(frag_uncon, key=lambda x: x.get_atom_count())
+        frag_uncon_deg1 = sorted(frag_uncon_deg1, key=lambda x: x.get_atom_count())
+
+        self.assertEqual(frag_uncon[0].get_atom_count(), mol1.get_atom_count())
+        self.assertEqual(frag_uncon_deg1[0].get_atom_count(), mol1.get_atom_count())
+
+    def test_mcf_subisomorphic_graphs_1(self):
+        from mogli import Molecule, maximal_common_fragments, GenerationType
+
+        mol1, mol2 = Molecule(), Molecule()
+
+        mol1.read_lgf(ETHYL)
+        mol2.read_lgf(ETHANE_1)
+
+        frag_noopt, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.NO_OPT, False, TIMEOUT)
+        frag_deg1, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.DEG_1, False, TIMEOUT)
+        frag_uncon, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON, False, TIMEOUT)
+        frag_uncon_deg1, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON_DEG_1, False, TIMEOUT)
+
+        self.assertTrue(len(frag_noopt) > 0)
+        self.assertTrue(len(frag_deg1) > 0)
+        self.assertTrue(len(frag_uncon) > 0)
+        self.assertTrue(len(frag_uncon_deg1) > 0)
+
+        frag_noopt = sorted(frag_noopt, key=lambda x: x.get_atom_count())
+        frag_deg1 = sorted(frag_deg1, key=lambda x: x.get_atom_count())
+        frag_uncon = sorted(frag_uncon, key=lambda x: x.get_atom_count())
+        frag_uncon_deg1 = sorted(frag_uncon_deg1, key=lambda x: x.get_atom_count())
+
+        self.assertEqual(frag_noopt[0].get_atom_count(), mol1.get_atom_count())
+        self.assertEqual(frag_deg1[0].get_atom_count(), mol1.get_atom_count())
+        self.assertEqual(frag_uncon[0].get_atom_count(), mol1.get_atom_count())
+        self.assertEqual(frag_uncon_deg1[0].get_atom_count(), mol1.get_atom_count())
+
+    def test_mcf_subisomorphic_graphs_2(self):
+        from mogli import Molecule, maximal_common_fragments, GenerationType
+
+        mol1, mol2 = Molecule(), Molecule()
+
+        mol1.read_lgf(ETHYL)
+        mol2.read_lgf(ETHANE_2)
+
+        frag_noopt, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.NO_OPT, False, TIMEOUT)
+        frag_deg1, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.DEG_1, False, TIMEOUT)
+        frag_uncon, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON, False, TIMEOUT)
+        frag_uncon_deg1, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON_DEG_1, False, TIMEOUT)
+
+        self.assertTrue(len(frag_noopt) > 0)
+        self.assertTrue(len(frag_deg1) > 0)
+        self.assertTrue(len(frag_uncon) > 0)
+        self.assertTrue(len(frag_uncon_deg1) > 0)
+
+        frag_noopt = sorted(frag_noopt, key=lambda x: x.get_atom_count())
+        frag_deg1 = sorted(frag_deg1, key=lambda x: x.get_atom_count())
+        frag_uncon = sorted(frag_uncon, key=lambda x: x.get_atom_count())
+        frag_uncon_deg1 = sorted(frag_uncon_deg1, key=lambda x: x.get_atom_count())
+
+        self.assertEqual(frag_noopt[0].get_atom_count(), mol1.get_atom_count())
+        self.assertEqual(frag_deg1[0].get_atom_count(), mol1.get_atom_count())
+        self.assertEqual(frag_uncon[0].get_atom_count(), mol1.get_atom_count())
+        self.assertEqual(frag_uncon_deg1[0].get_atom_count(), mol1.get_atom_count())
+
+    def test_mcf_graphs_no_match(self):
+        from mogli import Molecule, LGFIOConfig, maximal_common_fragments, GenerationType
+
+        mol1, mol2 = Molecule(), Molecule()
+
+        config = LGFIOConfig('label', 'atomType')
+        mol1.read_lgf(ETHANE_1)
+        mol2.read_lgf(GENERIC_1, config)
+
+        frag_noopt, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.NO_OPT, False, TIMEOUT)
+        frag_deg1, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.DEG_1, False, TIMEOUT)
+        frag_uncon, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON, False, TIMEOUT)
+        frag_uncon_deg1, _, _ = maximal_common_fragments(mol1, mol2, 1, 0, GenerationType.UNCON_DEG_1, False, TIMEOUT)
+
+        self.assertEqual(len(frag_noopt), 0)
+        self.assertEqual(len(frag_deg1), 0)
+        self.assertEqual(len(frag_uncon), 0)
+        self.assertEqual(len(frag_uncon_deg1), 0)
+
+    def test_atomic_fragments(self):
+        from mogli import Molecule, atomic_fragments
+
+        mol = Molecule()
+        mol.read_lgf(ETHANE_1)
+
+        frag0, _ = atomic_fragments(mol, 0)
+        frag1, _ = atomic_fragments(mol, 1)
+        frag2, _ = atomic_fragments(mol, 2)
+
+        self.assertEqual(len(frag0), 8)
+        self.assertEqual(len(frag1), 8)
+        self.assertEqual(len(frag2), 8)
 
 
 class TestPacking(unittest.TestCase):
